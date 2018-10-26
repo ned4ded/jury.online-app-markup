3ae4ea90964db05f2192d78342219049414f5c2e
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-10-26 13:59:11 +0300

    add js for side-bar toggler

---
+++ src/scripts/terminal.js
@@ -0,0 +1,38 @@
+(() => {
+  const t = document.querySelector('[data-navbar-toggle]');
+
+  const l = document.getElementById('lining');
+
+  const navbar = document.querySelector('.navbar');
+  const sidemenu = document.querySelector('.dashboard__side-menu');
+
+  const targets = [l , navbar, sidemenu];
+
+  const handler = fn => ev => {
+    ev.preventDefault();
+
+    fn();
+  }
+
+  const toggleHandler = (ev) => handler(() => {
+    t.removeEventListener('click', toggleHandler);
+
+    l.addEventListener('click', liningHandler);
+
+    targets.forEach(t => t.dataset.state = 'open');
+
+    return;
+  })(ev);
+
+  const liningHandler = (ev) => handler(() => {
+    l.removeEventListener('click', liningHandler);
+
+    t.addEventListener('click', toggleHandler);
+
+    targets.forEach(t => t.dataset.state = 'close');
+
+    return;
+  })(ev);
+
+  t.addEventListener('click', toggleHandler);
+})();
