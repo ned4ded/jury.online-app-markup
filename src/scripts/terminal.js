cee1183fb402fe5b594df95d6fc796dc8d1ce722
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-11-01 16:14:39 +0300

    add the swiper integration and the carousel on the dashboard page

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -3,8 +3,14 @@ import './tooltip';
 import './select';
 import './tags';
 import './changeToggle';
+import swiper from './vendor/swiper';
 
-(() => {
+
+$(document).ready(function () {
+  const body = document.querySelector('body');
+
+  body.classList.remove('no-js');
+
   const t = document.querySelector('[data-navbar-toggle]');
 
   const l = document.getElementById('lining');
@@ -41,4 +47,6 @@ import './changeToggle';
   })(ev);
 
   t.addEventListener('click', toggleHandler);
-})();
+
+  swiper();
+});
---
+++ src/scripts/vendor/swiper.js
@@ -0,0 +1,17 @@
+import Swiper from 'swiper';
+import { NAVBAR_COLLAPSE_BP, WIN_WIDTH } from '../constants';
+
+export default function() {
+  if(WIN_WIDTH >= NAVBAR_COLLAPSE_BP) {
+    Array.from(document.querySelectorAll('[data-carousel="swiper"]')).forEach(n => new Swiper(n, {
+      loop: false,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      initialSlide: 1,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }));
+  }
+}
