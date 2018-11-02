297d1763b6f7ba6d1c9db193b126dc47c8bc687f
Валерий <kaktuz34@gmail.com>
2018-11-02 13:51:03 +0200

    added sort

--- src/scripts/projectAccordeon.js
+++ src/scripts/projectAccordeon.js
@@ -4,16 +4,12 @@ trigger.on('click', function() {
   const that = $(this);
   const open = 'is-open-project';
   const id = that.data('project-trigger');
-  const blocks = $('[data-project-id]');
   const thisBlock = $(`[data-project-id="${id}"]`);
   if (!that.hasClass(open)) {
-    trigger.removeClass(open);
     that.addClass(open);
-    blocks.slideUp();
     thisBlock.slideDown();
   }
   else {
-    console.log('www');
     that.removeClass(open);
     thisBlock.slideUp();
   }
