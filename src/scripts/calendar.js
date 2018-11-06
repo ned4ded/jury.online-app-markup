801d1a311e033bff4f209e156d1ce3a6d92b9b01
Валерий <kaktuz34@gmail.com>
2018-11-06 14:11:49 +0200

    update icon-calendar

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,20 +1,34 @@
 import datepicker from 'air-datepicker';
 import { OPEN, BODY } from './constants';
-const trigger = $('.js-icon-calendar-trigger');
-const input = $('.js-icon-calendar-input');
-const parentWrap = '.js-icon-calendar';
 
-var flag = false;
-input.datepicker({
-	onSelect(formattedDate, date, inst) {
-		const parent = $(inst.el).parents(parentWrap);
-		if (date.length === 2) {
-			parent.removeClass(OPEN);
-			flag = true;
+const parentWrap = $('.js-icon-calendar');
+parentWrap.each((i,el) => {
+	const parent = $(el);
+	const input = $('.js-icon-calendar-input', parent);
+	const startText = input.data('placeholder');
+	const startDate = input.data('start-select-date');
+	const endDate = input.data('end-select-date');
+	
+	let dp = input.datepicker({
+		dateFormat: 'dd.mm.yyyy',
+		onSelect(formattedDate, date, inst) {
+			const textWrap = parent.find('.js-icon-calendar-text');
+
+			if (date.length === 2)  parent.removeClass(OPEN);
+
+			(formattedDate)
+				? textWrap.text(formattedDate)
+				: textWrap.text(startText);
 		}
+	}).data('datepicker');
+
+	if (startDate && endDate) {
+		dp.selectDate(new Date(startDate));
+		dp.selectDate(new Date(endDate));
 	}
 });
 
+const trigger = $('.js-icon-calendar-trigger');
 trigger.on('click', function(){
 	const that = $(this);
 	const parent = that.parents(parentWrap);
