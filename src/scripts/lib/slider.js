96878bb2e663857320266959af1ad79bac7564a4
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-11-07 18:17:08 +0300

    slider's scripts are completely done

--- src/scripts/lib/slider.js
+++ src/scripts/lib/slider.js
@@ -54,6 +54,8 @@ class Span extends Element {
 class Input extends Element {
   constructor(el) {
     super(el);
+
+    this._init = this._el.attr('value') || 0;
   }
 
   setValue(value) {
@@ -62,6 +64,14 @@ class Input extends Element {
 
   getValue() {
     return this._el.val();
+  }
+
+  getCap() {
+    return this._el.attr(Attrs.INPUT);
+  }
+
+  getInit() {
+    return this._init;
   }
 
   change(fn) {
@@ -103,41 +113,133 @@ class ValueHandler {
     return this._input.getValue();
   }
 
+  getInit() {
+    return this._input.getInit();
+  }
+
+  getCap() {
+    return this._input.getCap();
+  }
+
   _handler = (ev, value) => {
     this._span.text(value);
   }
 }
 
 class ValueHandlers {
-  constructor(arr) {
+  constructor() {
+    this._map = new Map();
+  }
 
+  add(type, h) {
+    if(this.has(type)) throw new Error(`ValueHandlers: Already have such a type ${type}`);
+
+    this._map.set(type, h);
+
+    return this;
+  }
+
+  has(key) {
+    return this._map.has(key);
+  }
+
+  get(key) {
+    return this._map.get(key);
+  }
+
+  _process(fn, filter = () => true) {
+    const entries = this._map.entries();
+
+    for (let [key, value] of entries) {
+      if(filter(key, value)) {
+        fn(null, key, value);
+      };
+    }
+
+    return fn(true);
+  }
+
+  getInit() {
+    const inits = () => {
+      const obj = {};
+
+      return (end, type, handler) => {
+        if(end) {
+          return obj;
+        }
+
+        obj[type] = Number(handler.getInit());
+
+        return;
+      };
+    };
+
+    return this._process(inits());
+  }
+
+  getMinMax() {
+    const values = () => {
+      const obj = {};
+
+      return (end, type, handler) => {
+        if(end) {
+          return obj;
+        }
+
+        obj[type] = Number(handler.getCap());
+
+        return;
+      };
+    };
+
+    return this._process(values());
+  }
+
+  update(type, value) {
+    const update = (end, type, handler) => {
+      return end? null : handler.update(value);
+    }
+
+    const filter = (key, value) => {
+      return key === type;
+    };
+
+    this._process(update, filter);
+
+    return this;
   }
 }
+
+const validateType = (type) => {
+  return Object.values(Values).includes(type);
+}
+
+const makeValueHandlers = (containers) => {
+  const rec = (arr, acc) => {
+    const [first, ...rest] = arr;
+
+    const input = first.querySelector(Selectors.INPUT);
+    const span = first.querySelector(Selectors.AMOUNT);
+
+    const type = $(first).attr(Attrs.VALUE);
+    if(!validateType(type)) throw new TypeError(`Slider: wrong type of min/max values: ${type}`);
+
+    const handlers = acc.add(type, new ValueHandler({ input, span }));
+
+    return rest.length ? rec(rest, handlers) : handlers;
+  }
+
+  return rec(containers, new ValueHandlers());
+}
+
 
 class Slider {
   constructor(el, options) {
     this._el = el;
 
-    const values = $( this._el ).find(Selectors.VALUE).get().map(e => {
-      const input = e.querySelector(Selectors.INPUT);
+    const valueHandlersContainers = $( this._el ).find(Selectors.VALUE).get();
 
-      const span = e.querySelector(Selectors.AMOUNT);
-
-      const type = (() => {
-        switch ($(e).attr(Attrs.VALUE)) {
-          case Values.MIN:
-            return Values.MIN;
-          case Values.MAX:
-            return Values.MAX;
-          default:
-            throw new TypeError(`Value type of ${Attrs.VALUE} is wrong`);
-        };
-
-        return;
-      })();
-
-      return new ValueHandler({ input, span });
-    });
+    this._valueHandlers = makeValueHandlers(valueHandlersContainers);
 
     const container = $( el ).find(Selectors.CONTAINER);
 
@@ -145,21 +247,19 @@ class Slider {
 
     const opts = {
       ...options,
-      min: 0,
-      max: 100,
-      values: [values[0].getValue(), values[1].getValue()]
+      ...this._valueHandlers.getMinMax(),
+      step: 0.01,
+      values: Object.values(this._valueHandlers.getInit())
     }
 
-    this._container = $( container ).slider(opts).on( "slide", function( ev, ui ) {
-      values[0].update(ui.values[0]);
-      values[1].update(ui.values[1]);
+    this._container = $( container ).slider(opts).on( "slide", ( ev, ui ) => {
+      const [ min, max ] = ui.values;
+
+      this._valueHandlers.update(Values.MIN, min).update(Values.MAX, max);
+
+      return;
     } );
   }
-
-  _uploadValueHandlers() {
-
-  }
-
 }
 
 export default function() {
