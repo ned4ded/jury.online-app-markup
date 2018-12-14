5e6f397eb609cad24af0b1c03e8c5073591f22f9
Валерий <kaktuz34@gmail.com>
2018-12-14 11:57:42 +0200

    fixed swiper-slider

--- src/scripts/lib/carousel.js
+++ src/scripts/lib/carousel.js
@@ -13,5 +13,19 @@ export default function() {
         nextEl: '.swiper-button-next',
       },
     }));
+    const slides = document.querySelectorAll('[data-carousel="swiper-attach"] .swiper-slide');
+    const sliders = document.querySelectorAll('[data-carousel="swiper-attach"]');
+    if (slides.length <=3) return;
+    $(sliders).addClass('is-init');
+    Array.from(sliders).forEach(n => new Swiper(n, {
+      loop: true,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      initialSlide: 2,
+      navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+      },
+    }));
   }
 }
