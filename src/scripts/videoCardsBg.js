25a7d4952dd342f52156befb1cd03e7de3162b01
Валерий <kaktuz34@gmail.com>
2018-12-18 13:26:38 +0200

    fixed card bg-video

--- src/scripts/videoCardsBg.js
+++ src/scripts/videoCardsBg.js
@@ -10,9 +10,15 @@ export default function() {
     players.each((i,el)=>{
       let player = new YT.Player(el, {
         events: {
-            'onReady': function(){
-              onPlayerReady(i,el)
+          'onReady': function(){
+            onPlayerReady(i,el);
+          },
+          'onStateChange': function(obj){
+            if (obj.data === 1 && !$(el).hasClass('is-active')) {
+              $(el).addClass('is-active');
+              playersArray[i].pauseVideo();
             }
+          }
         }
       });
       playersArray.push(player);
@@ -20,13 +26,15 @@ export default function() {
   }
   const onPlayerReady = (i,el)=> {
     playersArray[i].mute();
-    playersArray[i].hideVideoInfo()
+    playersArray[i].playVideo();
+    playersArray[i].hideVideoInfo();
 
     const parent = $(el).parents('.js-cards-bg');
     parent.hover(function(){
+      parent.addClass('is-active');
       playersArray[i].playVideo();
     }, function(){
       playersArray[i].pauseVideo();
-    })
+    });
   }
 };
